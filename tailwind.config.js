dc12094d71317485382f372579ae9c0ffc733d18
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2023-09-11 18:49:18 +0700

    Initiated tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    './src/**/*.{html,js,jsx}',
+    './src/components/**/*.{html,js,jsx}'
+  ],
   theme: {
     extend: {},
   },
